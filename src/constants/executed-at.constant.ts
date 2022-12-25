2374210992c023b5a93a4e7cc96aebe812cbbdef
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-12-26 01:30:28 +0800

    Refactor executed at return value

--- src/constants/executed-at.constant.ts
+++ src/constants/executed-at.constant.ts
@@ -2,7 +2,7 @@ Object.defineProperty(globalThis, "executedAt", {
   get(): string {
     const stack: { stack?: string } = {};
     Error.captureStackTrace(stack);
-    return `(${stack.stack?.split("\n")[3]?.split("/")?.at(-1) ?? ""})`;
+    return `(${stack.stack?.split("\n")[3]?.split("/")?.at(-1) ?? ""}`;
   },
 });
 
